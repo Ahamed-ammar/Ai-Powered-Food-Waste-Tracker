c5f1296a3f6851f41524433a5e3f99e4c43ebb5f
Ahamed-ammar <ahamedammar25@gmail.com>
2025-11-14 15:05:43 +0530

    Initial Commit

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -9,14 +9,14 @@ router.post('/register', async (req, res) => {
   try {
     const { name, email, password, role, organizationType } = req.body;
     
-    let user = await User.findOne({ email });
+    let user = await User.findOne( { email } );
     if (user) {
       return res.status(400).json({ message: 'User already exists' });
     }
 
     const hashedPassword = await bcrypt.hash(password, 10);
     
-    user = new User({
+    user = new User ({
       name,
       email,
       password: hashedPassword,
